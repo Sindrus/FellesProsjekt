4722ac2511abcecf3a17a60ad310c77f4f37ca44
x10an14 <x10an14@hotmail.com>
2012-03-15 13:07:28 +0100

    Proever aa legge til paanytt igjen GUI og login.Java

---
+++ src/GUI/Login.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package GUI;
+
+/**
+ * @author x10an14
+ *
+ */
+public class Login {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
