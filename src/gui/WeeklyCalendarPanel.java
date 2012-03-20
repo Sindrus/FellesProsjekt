4793ee36ee2b879c56161a6c73792961de14d04f
Nenad Zikic <nenadfs@hotmail.com>
2012-03-20 14:23:11 +0100

    Wrote the logic for days, months, etc

--- src/gui/WeeklyCalendarPanel.java
+++ src/gui/WeeklyCalendarPanel.java
@@ -12,9 +12,20 @@ import java.util.Calendar;
 
 public class WeeklyCalendarPanel {
 	
-	int weeknum = Calendar.getInstance().WEEK_OF_MONTH;
-
+	
 	public static void main(String args[]) {
-		 System.out.println(Calendar.getInstance().getTime()); //<----Displays Time (Day(week),month,day(number),time,timezone,year.
-		}
+		
+		Calendar c = Calendar.getInstance();
+		String[] weekdays = {"Mandag", "Tirsdag", "Onsdag", "Torsdag", "Fredag", "Lordag", "Sondag"};
+		String[] monthnames = {"Januar", "Februar", "Mars", "April", "Mai", "Juni", "Juli", "August", "Semptember", "Oktober", "November", "Desember"};
+		int weeknum = c.get(Calendar.WEEK_OF_YEAR);
+		int daynum = c.get(Calendar.DAY_OF_MONTH);
+		int monthnum = Calendar.MONTH+1;
+		
+		System.out.println(weeknum);  // Proper use. Week number.
+		System.out.println(daynum); // Day number.
+	 	System.out.println(monthnum); // Month number.
+		System.out.println(weekdays[c.get(Calendar.DAY_OF_WEEK)-2]); //Proper use. Day of the week (Name).
+		System.out.println(monthnames[c.get(Calendar.MONTH)]); //Proper use. Month of the year (Name).
 	}
+}
