2f9ed923f1d015874c301ef1ba26e6d21f07f246
Sindre <sindreny@gmail.com>
2012-03-21 10:52:18 +0100

    Sorry for late push, anyways, done with newMeetingPanel and newAppointmentPanel

--- src/gui/NewAppointmentPanel.java
+++ src/gui/NewAppointmentPanel.java
@@ -1,122 +1,179 @@
 package gui;
 
+/**
+ * -*- coding:utf-8 -*-
+ */
+
 import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.beans.PropertyChangeSupport;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
+import util.GuiListenerSupport;
+
 public class NewAppointmentPanel extends JPanel {
-	
-	JButton Avbryt, Full, Invite;
-	JTextField Hva,Beskrivelse;
-	JComboBox Dag, Maned, Stid, Etid;
-	PropertyChangeSupport pcs;
 
-	public NewAppointmentPanel(){
+	JButton cancel, complete, invite;
+	JPanel whatPanel, whenPanel, desPanel, btnPanel;
+	JTextField what;
+	JTextArea description;
+	JComboBox day, month, startTime, endTime;
+	GuiListenerSupport gls;
 
+	public NewAppointmentPanel() {
 		String[] td = new String[24];
-		for (int i = 0; i<=23; i++)
-		{
-		if (i<10)
-			td[i] = "0" + i + ":00";
-		else
-			td[i] = i + ":00";
+		for (int i = 0; i <= 23; i++) {
+			if (i < 10)
+				td[i] = "0" + i + ":00";
+			else
+				td[i] = i + ":00";
 		}
-		
-		String[] mnd = {"Januar", "Februar", "Mars", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Desember"};
-		
+
+		String[] mnd = { "Januar", "Februar", "Mars", "April", "Mai", "Juni",
+				"Juli", "August", "September", "Oktober", "November",
+				"Desember" };
+
 		String[] dag = new String[32];
-		for (int i = 0; i<=31; i++)
-		{
-		dag[i] = Integer.toString(i);
+		for (int i = 0; i <= 31; i++) {
+			dag[i] = Integer.toString(i);
 		}
-		
-		pcs = new PropertyChangeSupport(this);
+
+		gls = new GuiListenerSupport();
 		setLayout(new GridBagLayout());
 		GridBagConstraints g = new GridBagConstraints();
-		
-		g.anchor = GridBagConstraints.WEST;
-		g.gridy=0;
-		JLabel h = new JLabel("Hva:");
-		add(h,g);
-		Hva = new JTextField(""); 
-		Font f = new Font(Hva.getFont().getName(),Hva.getFont().getStyle(),14);
-		Hva.setColumns(20);
-		Hva.setFont(f);
-		add(Hva,g);
+		g.anchor = GridBagConstraints.NORTHWEST;
+		g.insets = new Insets(5, 5, 5, 5);
 
-		g.anchor = GridBagConstraints.WEST;
+// whatPanel
+		whatPanel = new JPanel();
+		GridBagConstraints whatg = new GridBagConstraints();
+		whatg.anchor = GridBagConstraints.WEST;
+
+		whatg.gridy = 0;
+		whatg.gridx = 0;
+		JLabel h = new JLabel("Hva:");
+		whatPanel.add(h, whatg);
+		what = new JTextField("");
+		Font f = new Font(what.getFont().getName(), what.getFont().getStyle(),
+				14);
+		what.setColumns(20);
+		what.setFont(f);
+		whatPanel.add(what, whatg);
+
+// end whatPanel
+		g.gridx = 0;
+		g.gridy = 0;
+		add(whatPanel, g);
+
+// whenPanel
+		whenPanel = new JPanel();
+		GridBagConstraints wheng = new GridBagConstraints();
+
+		whenPanel.add(new JLabel("NÃ¥r:"), whatg);
+
+		wheng.anchor = GridBagConstraints.WEST;
+		wheng.gridx = 0;
+		wheng.gridy = 0;
+		whenPanel.add(new JLabel("Dag:"), wheng);
+
+		wheng.gridx = 1;
+		day = new JComboBox(dag);
+		whenPanel.add(day, wheng);
+
+		wheng.gridx = 2;
+		whenPanel.add(new JLabel("MÃ¥ned:"), wheng);
+
+		wheng.gridx = 3;
+		month = new JComboBox(mnd);
+		whenPanel.add(month, wheng);
+
+		wheng.gridx = 4;
+		whenPanel.add(new JLabel("Fra:"), wheng);
+
+		wheng.gridx = 5;
+		startTime = new JComboBox(td);
+		whenPanel.add(startTime, wheng);
+
+		wheng.gridx = 6;
+		whenPanel.add(new JLabel("Til:"), wheng);
+
+		wheng.gridx = 7;
+		endTime = new JComboBox(td);
+		whenPanel.add(endTime, wheng);
+
+// end whenPanel
 		g.gridy = 1;
-		JLabel n = new JLabel("Når:");
-		add(n,g);
+		add(whenPanel, g);
+
+// desPanel
+		desPanel = new JPanel();
+		desPanel.setLayout(new GridBagLayout());
+		GridBagConstraints desg = new GridBagConstraints();
+		desg.anchor = GridBagConstraints.NORTHWEST;
+		desg.gridx = 0;
+		desg.gridy = 0;
+
+		desPanel.add(new JLabel("Beskrivelse:"), desg);
+
+		description = new JTextArea(4, 30);
+		description.setFont(f);
+		desg.gridx = 1;
+		desPanel.add(description, desg);
+
+// end desPanel
+		g.gridy = 2;
+		add(desPanel, g);
+
+// btnPanel
+		btnPanel = new JPanel();
+		btnPanel.setLayout(new GridBagLayout());
+		GridBagConstraints btng = new GridBagConstraints();
+		btng.anchor = GridBagConstraints.CENTER;
+		btng.gridx = 0;
+		btng.gridy = 0;
+		btng.insets = new Insets(0, 30, 0, 30);
+
+		complete = new JButton("FullfÃ¸rt");
+		complete.addActionListener(new Decline());
+		btnPanel.add(complete, btng);
+
+		btng.insets.left = 5;
+		btng.gridx = 1;
+		cancel = new JButton("Avbryt");
+		cancel.addActionListener(new Accept());
+		btnPanel.add(cancel, btng);
+
+		btng.gridx = 2;
+		btng.insets.right = 0;
+		invite = new JButton("Inviter til mÃ¸te");
+		invite.addActionListener(new Decline());
+		btnPanel.add(invite, btng);
 		
-		JLabel d = new JLabel("Dag:");
-		add(d,g);
-		Dag = new JComboBox(dag);
-		add(Dag,g);
-		
-		JLabel m = new JLabel("Måned:");
-		add(m,g);
-		Maned = new JComboBox(mnd);
-		add(Maned,g);
-		
-		JLabel st = new JLabel("Fra:");
-		add(st,g);
-		Stid = new JComboBox(td);
-		add(Stid,g);
-		
-		JLabel et = new JLabel("Til:");
-		add(et,g);
-		Etid = new JComboBox(td);
-		add(Etid,g);
-		
-		g.anchor = GridBagConstraints.WEST;
-		g.gridy=2;
-		JLabel b = new JLabel("Beskrivelse:");
-		add(b,g);
-		Beskrivelse = new JTextField("");
-		Beskrivelse.setFont(f);
-		Beskrivelse.setColumns(50);
-		add(Beskrivelse,g);
-		
-		g.anchor = GridBagConstraints.WEST;
-		g.gridy=3;
-		Avbryt = new JButton("Avbryt");
-		Avbryt.addActionListener(new Godta());
-		add(Avbryt,g);
-		
+// end btnPanel
 		g.anchor = GridBagConstraints.CENTER;
-		g.gridy=3;
-		Full = new JButton("Fullført");
-		Full.addActionListener(new Avvis());
-		add(Full,g);
-		
-		g.anchor = GridBagConstraints.EAST;
-		g.gridy=3;
-		Invite = new JButton("Inviter til møte");
-		Invite.addActionListener(new Avvis());
-		add(Invite,g);
+		g.gridy = 3;
+		add(btnPanel, g);
 	}
-	
-	
-	class Godta implements ActionListener { //no logic.
+
+	class Accept implements ActionListener { // no logic.
 		public void actionPerformed(ActionEvent e) {
 		}
 	}
-	
-	class Avvis implements ActionListener { //no logic.
+
+	class Decline implements ActionListener { // no logic.
 		public void actionPerformed(ActionEvent e) {
 		}
 	}
-	
+
 	public static void main(String args[]) {
 		JFrame frame = new JFrame();
 		frame.add(new NewAppointmentPanel());
--- src/gui/NewMeetingPanel.java
+++ src/gui/NewMeetingPanel.java
@@ -1,5 +1,20 @@
 package gui;
 
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.DefaultListModel;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.ListSelectionModel;
+
 /**
  * 
  * @author sindre
@@ -8,6 +23,110 @@ package gui;
  * 
  */
 
-public class NewMeetingPanel {
+public class NewMeetingPanel extends JPanel{
+	
+	JList personList, romList;
+	JButton backBtn, createBtn;
+	DefaultListModel defaultPersonListModel, defaultRomListModel;
+	JPanel innerPanel, btnPanel;
+	
+	public NewMeetingPanel(){
 
+		setLayout(new GridBagLayout());
+		GridBagConstraints g = new GridBagConstraints();
+		g.anchor = GridBagConstraints.CENTER;
+		g.insets = new Insets(5, 5, 5, 5);
+		g.weightx = 0.5;
+		g.weighty = 0.5;
+		
+//	innerPanel
+		GridBagConstraints parg = new GridBagConstraints();
+		parg.anchor = GridBagConstraints.NORTHWEST;
+		parg.insets = new Insets(5, 5, 5, 5);
+		parg.gridx=0;
+		parg.gridy=0;
+		
+		innerPanel = new JPanel(new GridBagLayout());
+		
+		innerPanel.add(new JLabel("Velg deltakere: "),parg);
+		
+
+		defaultPersonListModel = new DefaultListModel();
+		personList = new JList(defaultPersonListModel);
+		personList.setVisibleRowCount(5);
+		personList.setFixedCellWidth(300);
+		personList.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+		parg.gridx=1;
+		innerPanel.add(personList,parg);
+		
+		JScrollPane personScroll = new JScrollPane(personList);
+		personScroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+		innerPanel.add(personScroll,parg);
+		
+		parg.gridx=0;
+		parg.gridy=1;
+		
+		innerPanel.add(new JLabel("Reserver rom: "),parg);
+		
+		defaultRomListModel = new DefaultListModel();
+		romList = new JList(defaultRomListModel);
+		romList.setVisibleRowCount(5);
+		romList.setFixedCellWidth(200);
+		romList.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+		parg.gridx=1;
+		innerPanel.add(romList,parg);
+		
+		JScrollPane romScroll = new JScrollPane(romList);
+		romScroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+		innerPanel.add(romScroll,parg);
+		
+//	end innerPanel
+		g.gridy=0;
+		add(innerPanel,g);
+		
+//	btnPanel
+		btnPanel = new JPanel(new GridBagLayout());
+		
+		GridBagConstraints btng = new GridBagConstraints();
+		btng.anchor = GridBagConstraints.CENTER;
+		btng.insets = new Insets(0,30,0,30);
+		btng.gridx=0;
+		btng.gridy=0;
+		
+		createBtn = new JButton("Fullfør");
+		createBtn.addActionListener(new createAction());
+		btng.gridx=0;
+		btnPanel.add(createBtn,btng);
+		
+		backBtn = new JButton("Tilbake");
+		backBtn.addActionListener(new backAction());
+		btng.gridx=1;
+		btnPanel.add(backBtn,btng);
+		
+//	end btnPanel
+		g.gridy=1;
+		add(btnPanel,g);
+		
+	}
+	
+	class backAction implements ActionListener{
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			// TODO Auto-generated method stub
+		}
+	}
+	class createAction implements ActionListener{
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+		}
+	}
+	
+	public static void main(String args[]) {
+		JFrame frame = new JFrame();
+		frame.setSize(700,700);
+		frame.add(new NewMeetingPanel());
+		frame.pack();
+		frame.setVisible(true);
+	}
 }
